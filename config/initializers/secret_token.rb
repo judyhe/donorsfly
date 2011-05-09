5e64e19c33127006b974f73ab197f22e82db3ccb
judyhe <judy.w.he@gmail.com>
2011-05-08 20:52:56 -0400

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Donorsfly::Application.config.secret_token = '12dd1304962b32342587f68f188d8afb5501b6c5bb2ff331fb457ddac1c105b823cabc69388ccefdfc66ceeffb1559948a3f1590b5b221c79da69376b7764ed4'
