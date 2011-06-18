9dc92293f0b40b4b9898cc472dc7ac326be06b35
judyhe <judy.w.he@gmail.com>
2011-06-17 23:01:28 -0400

    add the stuff from last week

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     @ip = request.remote_ip
     @geo = DonorsFly::SimpleGeo.ip_to_geo(@ip)
-    @proposals = DonorsFly::DonorsChoose.get_proposals_nearby(@geo, max=1)
+    @proposals = DonorsFly::DonorsChoose.get_proposals_nearby(@geo)
   end
   
 end
